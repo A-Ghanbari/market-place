818b8557c79c7ea0d579bfd29cc9257750d3b373
A-Ghanbari <a893gh@gmail.com>
2023-01-09 15:35:08 +0330

    take photo

--- src/components/auth/camera/index.js
+++ src/components/auth/camera/index.js
@@ -1,10 +1,15 @@
-import "components/auth/camera/camera.scss"
 import { useEffect, useRef, useState } from "react"
+import "components/auth/camera/camera.scss"
 
 export default function Camera() {
     const videoRef = useRef(null)
     const photoRef = useRef(null)
     const [hasPhoto, setHasPhoto] = useState(false)
+
+    useEffect(() => {
+        getVideo()
+    }, [videoRef])
+
     const getVideo = () => {
         navigator.mediaDevices
             .getUserMedia({
@@ -19,19 +24,41 @@ export default function Camera() {
                 console.error(err);
             })
     }
-    useEffect(() => {
-        getVideo()
-    }, [videoRef])
+
+    const takePhoto = () => {
+        const width = 414
+        const height = width / (16 / 9)
+
+        let video = videoRef.current
+        let photo = photoRef.current
+
+        photo.width = width
+        photo.height = height
+
+        let ctx = photo.getContext('2d')
+        ctx.drawImage(video, 0, 0, width, height)
+        setHasPhoto(true)
+
+    }
+
+    const closePhoto = () => {
+        let photo = photoRef.current
+        let ctx = photo.getContext('2d')
+
+        ctx.clearRect(0, 0, photo.width, photo.height)
+        setHasPhoto(false)
+
+    }
 
     return (
         <div className="wrapper">
             <div className="camera">
                 <video ref={videoRef} muted autoplay playsinline></video>
-                <button>SNAP!</button>
+                <button onClick={takePhoto}>SNAP!</button>
             </div>
-            <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+            <div className={'result ' + (hasPhoto ? 'hasPhoto' : '')}>
                 <canvas ref={photoRef}></canvas>
-                <button>CLOSE!</button>
+                <button onClick={closePhoto}>CLOSE!</button>
             </div>
         </div>
     )
